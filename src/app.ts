50115acd63bd5d038f43da2c3878eb9545c31f6b
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2023-07-03 23:15:43 +0600

    module-17 almost done

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
 import httpStatus from 'http-status';
+import cookieParser from 'cookie-parser';
 // import { generateFacultyId } from './app/modules/users/users.utils';
 
 const app: Application = express();
 
 // middleware
 app.use(cors());
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,9 +1,9 @@
 import { Schema, model } from 'mongoose';
-import { IUser, UserModel } from './users.interface';
+import { IUser, IUserMethods, UserModel } from './users.interface';
 import bcrypt from 'bcrypt';
 import config from '../../../config';
 
-const userSchema = new Schema<IUser>(
+const userSchema = new Schema<IUser, Record<string, never>, IUserMethods>(
   {
     id: {
       type: String,
@@ -17,6 +17,11 @@ const userSchema = new Schema<IUser>(
     password: {
       type: String,
       required: true,
+      select: 0,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
     },
     student: {
       type: Schema.Types.ObjectId,
@@ -38,6 +43,31 @@ const userSchema = new Schema<IUser>(
     },
   }
 );
+
+userSchema.methods.isUserExist = async function (
+  id: string
+): Promise<Partial<IUser> | null> {
+  const user = await User.findOne(
+    { id },
+    {
+      id: 1,
+      password: 1,
+      needsPasswordChange: 1,
+      role: 1,
+    }
+  );
+
+  return user;
+};
+
+userSchema.methods.isPasswordMatched = async function (
+  givenPassword: string,
+  savedPassword: string
+): Promise<boolean> {
+  const isMatched = await bcrypt.compare(givenPassword, savedPassword);
+
+  return isMatched;
+};
 
 userSchema.pre('save', async function (next) {
   // hashing user password
